eac290e46a95172490be37cb2e10eedd0996293d
DangHuuPhuocVinh <danghuu_phuocvinh@yahoo.com>
2024-05-03 18:24:52 +0700

    Add new project space game

---
+++ Space game/script.js
@@ -0,0 +1,20 @@
+const canvas = document.querySelector('canvas');
+
+const c = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(x: 12)
+    }
+}
