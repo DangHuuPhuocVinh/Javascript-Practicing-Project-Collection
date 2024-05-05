a13049402286c1c35f9f1ada69e168cad65ac22a
DangHuuPhuocVinh <danghuu_phuocvinh@yahoo.com>
2024-05-05 13:53:59 +0700

    Create class Enemy and add function sapwnEnemy

--- Space game/script.js
+++ Space game/script.js
@@ -44,20 +44,63 @@ class Projectile {
     }
 }
 
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+    }
+
+    update() {
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, 'red');
 
 const projectiles = [];
+const enemies = [];
 
-function animate() {
+function spawnEnemy(){
+    setInterval(() => {
+        const x = 100;
+        const y = 100;
+        const radius = 30;
+        const color = 'black';
+        const angle = Math.atan2(y - canvas.height / 2, x - canvas.width / 2);
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+    }, 1000)
+}
+
+function animate(){
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
     player.draw();
     projectiles.forEach(projectile => {
         projectile.update();
     })
+
+    enemies.forEach((enemy) => {
+        enemy.update();
+    }) 
 }
 
 addEventListener('click', (event) => {
@@ -70,3 +113,4 @@ addEventListener('click', (event) => {
 })
 
 animate();
+spawnEnemy();
