3342d56dce448328d7ece68b1815a565ae0bfd5b
DangHuuPhuocVinh <danghuu_phuocvinh@yahoo.com>
2024-05-08 12:10:51 +0700

    Complete enemy explosion effect

--- Space game/script.js
+++ Space game/script.js
@@ -67,6 +67,7 @@ class Enemy {
     }
 }
 
+const friction = 0.99;
 class Particle {
     constructor(x, y, radius, color, velocity) {
         this.x = x;
@@ -89,6 +90,8 @@ class Particle {
 
     update() {
         this.draw();
+        this.velocity.x *= friction;
+        this.velocity.y *= friction;
         this.x = this.x + this.velocity.x;
         this.y = this.y + this.velocity.y;
         this.alpha -= 0.01;
@@ -164,8 +167,8 @@ function animate() {
             if (dist - enemy.radius - projectile.radius < 1) {
                 for(let i = 0; i < enemy.radius * 2; i++){
                     particles.push(new Particle(projectile.x, projectile.y, Math.random() * 2, enemy.color, {
-                        x: Math.random() - 0.5,
-                        y: Math.random() - 0.5
+                        x: (Math.random() - 0.5) * (Math.random() * 6),
+                        y: (Math.random() - 0.5) * (Math.random() * 6)
                     }));
                 }
                 if (enemy.radius - 10 > 5) {
